bda9f5bea0944e33262626f51737254cd4222d51
AryaMourya <aryamourya0@gmail.com>
2026-07-22 19:28:52 +0530

    added program for adding new application and analyzing it

--- tracker.py
+++ tracker.py
@@ -15,3 +15,27 @@ def load_tracker():
         columns = ["Date Applied","Company","Job Title","Status","Platform","Location"]
         return pd.DataFrame(columns=columns)
     
+### Add a New Application
+def add_application(df, company, title, status, platform, location):
+   new_app = pd.DataFrame({
+       "Date Applied":[datetime.now().strftime("%Y-%m-%d")],
+       "Company":[company],
+       "Job Title":[title],
+       "Status":[status],
+       "Platform":[platform],
+       "Location":[location]
+   })
+   updated_df = pd.concat([df,new_app],ignore_index=True)
+   updated_df.to_csv(DATA_FILE,index=False)
+   print(f"Added {title} at {company}.")
+   return updated_df
+
+### Analyzing with Pandas & Numpy 
+def analyze_data(df):
+    if df.empty:
+        print("No job applications tracked yet.")
+        return
+
+
+df = load_tracker()
+unique_statuses = df["Status"].unique()
